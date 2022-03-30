f2687f82adc17f2c5f608dff4fa06c851fea0481
wramadan7-org <nielsenprambudiasallie@gmail.com>
2022-03-30 12:04:34 +0700

    set answer console.log, and test unit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const printLooping = () => {
+    alert("please check console.log")
+    for (var x=1; x <= 100; x++){
+      if( x % 3 && x % 5 ) {
+          console.log(x);
+      } else {
+          if( x % 3 == 0 ) {
+              console.log("fish");
+          }
+          if( x % 5 == 0 ) {
+              console.log("bash");
+          }
+      }
+    }
+  }
+
+  const printSort = () => {
+    alert("please check console.log")
+    var numbers = [5, 10, 15, 11, 14, 20, 16];
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < numbers.length; i += 1) {
+        if (numbers[i - 1] > numbers[i]) {
+          done = false;
+          var tmp = numbers[i - 1];
+          numbers[i - 1] = numbers[i];
+          numbers[i] = tmp;
+        }
+      }
+    }
+
+    console.log("numbers sort", numbers)
+  }
+
+  const checkReversed = () => {
+    alert("please check console.log")
+    var word = "cel";
+    var reverseWord = "lec";
+    var reverseWordFalse = "leci";
+    console.log("true condition", word.split("").reverse().join("") === reverseWord);
+    console.log("false condition", word.split("").reverse().join("") === reverseWordFalse);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button aria-label="A1" onClick={() => printLooping()}>
+          Jawaban nomor 1
+        </button>
+        <button aria-label="A2" style={{marginTop: 10}} onClick={() => printSort()}>
+          Jawaban nomor 2
+        </button>
+        <button aria-label="A3" style={{marginTop: 10}} onClick={() => checkReversed()}>
+          Jawaban nomor 3
+        </button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders button test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const answerElement1 = screen.getByRole('button', {name: /A1/i});
+  expect(answerElement1).toBeInTheDocument();
+  const answerElement2 = screen.getByRole('button', {name: /A2/i});
+  expect(answerElement2).toBeInTheDocument();
+  const answerElement3 = screen.getByRole('button', {name: /A3/i});
+  expect(answerElement3).toBeInTheDocument();
 });
